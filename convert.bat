cfef7862d8e6a7ba30b6e1f9cf3fab8539360373
raffaelj <github@raffaeljesche.de>
2016-06-18 22:32:06 +0200

    renaming of filenames, structured code, generates copy-paste-folder with index.md now, changed some variables

--- convert.bat
+++ convert.bat
@@ -1,3 +1,4 @@
+@echo off & setlocal
 :: image processing for galleries in jekyll blogs with NConvert on Windows 7
 :: 
 :: author: Raffael Jesche
@@ -37,64 +38,202 @@
 :: 
 :: Variablen // variables
 ::
-
 :: Pfad zu NConvert setzen // path/to/NConvert
-set nc=C:\ProgrammePortable\NConvert\nconvert.exe
+set "nc=C:\ProgrammePortable\NConvert\nconvert.exe"
 :: Bildgröße // image sizes
-set MediumX=500
-set MediumY=2000
-set LargeX=1000
-set LargeY=4000
-set ThumbsXY=150
+set "MediumX=500"
+set "MediumY=2000"
+set "LargeX=1000"
+set "LargeY=4000"
+set "ThumbsXY=150"
 :: jpg-Qualität // jpg quality
-set qualM=40
-set qualL=40
-set qualT=40
+set "qualM=40"
+set "qualL=40"
+set "qualT=25"
+:: Pfad zur Wasserzeichen-Datei
+set "watermark=watermark.png"
 :: Transparenz des Wasserzeichens // opacity of watermark
-set wmop=60
+set "wmop=50"
 
+:: Pfad zu Bildern für jekyll-Dateien
+set "imgpath={{site.galpath}}{{page.album_folder}}"
+
+:start
 :: Umlaute richtig darstellen (cmd-default: 850) // German umlauts
 chcp 1252
+echo.
+echo Image processing for jekyll galleries
+echo =====================================
+echo.
 
-:: 
-:: NConvert-Konvertierungen // NConvert conversions
-::
+call :tempfiles
+call :renaming
+call :nconvert
+call :jekyll
+call :removetempfiles
+goto :end
 
-:: medium, 500px breit, Qualität 40% // width: 500px
-%nc% -overwrite -out jpeg -o medium\%%.jpg -ratio -rtype lanczos -resize %MediumX% %MediumY% -q %qualM% -wmflag center -wmopacity %wmop% -wmfile watermark.png original\*.jpg
 
-:: large, 1000px breit, Qualität 40% // width: 1000px
-%nc% -overwrite -out jpeg -o large\%%.jpg -ratio -rtype lanczos -resize %LargeX% %LargeY% -q %qualL% -wmflag center -wmopacity %wmop% -wmfile watermark.png original\*.jpg
-
-:: Thumbnail 150x150 px // width & height: 150px
-%nc% -overwrite -out jpeg -o thumbs\%%.jpg -ratio -rtype lanczos -resize shortest %ThumbsXY% -q %qualT% original\*.jpg
-%nc% -overwrite -out jpeg -o thumbs\%%.jpg -canvas %ThumbsXY% %ThumbsXY% center thumbs\*.jpg
-
-::
-:: Dateien für jekyll erstellen // files for jekyll
-::
-
+:tempfiles
 :: Name des Ordners, in dem die Batch-Datei liegt // current dir of batch file
 for %%* in (.) do set CurrDirName=%%~nx*
 
 :: Leerzeichen in CurrDirName ersetzen // replace whitespaces
-set FolderName=%CurrDirName: =-%
+set "FolderName=%CurrDirName: =-%"
+set "FolderName=%FolderName:ä=ae%"
+set "FolderName=%FolderName:ö=oe%"
+set "FolderName=%FolderName:ü=ue%"
+set "FolderName=%FolderName:ß=ss%"
+
+:: original in temp kopieren
+echo copy originals into temp folder...
+set "tempFolder=temp-copy-of-original"
+xcopy /i /y original %tempFolder%
+
+goto :eof
+
+:renaming
+:: rename files in temp folder
+echo.
+echo renaming filenames...
+for /f "delims=" %%a in ('dir /a:-d /o:n /b %tempFolder%') do call :rename "%%a"
+::pause
+goto :eof
+
+:rename
+set "oldname=%~nx1"
+set "newname=%oldname%"
+
+set "newname=%newname: =-%"
+set "newname=%newname:)=_%"
+set "newname=%newname:(=_%"
+set "newname=%newname:&=_%"
+set "newname=%newname:^=_%"
+set "newname=%newname:$=_%"
+set "newname=%newname:#=_%"
+set "newname=%newname:@=_%"
+set "newname=%newname:!=_%"
+::set "newname=%newname:-=_%"
+set "newname=%newname:+=_%"
+set "newname=%newname:}=_%"
+set "newname=%newname:{=_%"
+set "newname=%newname:]=_%"
+set "newname=%newname:[=_%"
+set "newname=%newname:;=_%"
+set "newname=%newname:'=_%"
+set "newname=%newname:`=_%"
+set "newname=%newname:,=_%"
+
+set "newname=%newname:ä=ae%"
+set "newname=%newname:ö=oe%"
+set "newname=%newname:ü=ue%"
+set "newname=%newname:ß=ss%"
+set "newname=%newname:Ä=ae%"
+set "newname=%newname:Ö=oe%"
+set "newname=%newname:Ü=ue%"
+
+set "newname=%newname:A=a%"
+set "newname=%newname:B=b%"
+set "newname=%newname:C=c%"
+set "newname=%newname:D=d%"
+set "newname=%newname:E=e%"
+set "newname=%newname:F=f%"
+set "newname=%newname:G=g%"
+set "newname=%newname:H=h%"
+set "newname=%newname:I=i%"
+set "newname=%newname:J=j%"
+set "newname=%newname:K=k%"
+set "newname=%newname:L=l%"
+set "newname=%newname:M=m%"
+set "newname=%newname:N=n%"
+set "newname=%newname:O=o%"
+set "newname=%newname:P=p%"
+set "newname=%newname:Q=q%"
+set "newname=%newname:R=r%"
+set "newname=%newname:S=s%"
+set "newname=%newname:T=t%"
+set "newname=%newname:U=u%"
+set "newname=%newname:V=v%"
+set "newname=%newname:W=w%"
+set "newname=%newname:X=x%"
+set "newname=%newname:Y=y%"
+set "newname=%newname:Z=z%"
+
+::echo ren temp\%1 "%newname%
+ren "%tempFolder%\%oldname%" "%newname%"
+echo file renamed from "%oldname%" to "%newname%"
+echo.
+
+goto :eof
+
+:nconvert
+
+:: 
+:: NConvert-Konvertierungen // NConvert conversions
+::
+echo.
+
+:: medium, 500px breit, Qualität 40% // width: 500px
+%nc% -overwrite -out jpeg -o %FolderName%\medium\%%.jpg -ratio -rtype lanczos -resize %MediumX% %MediumY% -q %qualM% -wmflag center -wmopacity %wmop% -wmfile %watermark% %tempFolder%\*.jpg
+echo.
+echo created images (large)
+echo.
+
+:: large, 1000px breit, Qualität 40% // width: 1000px
+%nc% -overwrite -out jpeg -o %FolderName%\large\%%.jpg -ratio -rtype lanczos -resize %LargeX% %LargeY% -q %qualL% -wmflag center -wmopacity %wmop% -wmfile %watermark% %tempFolder%\*.jpg
+echo.
+echo created images (medium)
+echo.
+
+:: Thumbnail 150x150 px // width & height: 150px
+%nc% -overwrite -out jpeg -o %FolderName%\thumbs\%%.jpg -ratio -rtype lanczos -resize shortest %ThumbsXY% -q 100 %tempFolder%\*.jpg
+%nc% -overwrite -out jpeg -o %FolderName%\thumbs\%%.jpg -canvas %ThumbsXY% %ThumbsXY% center -q %qualT% %FolderName%\thumbs\*.jpg
+echo.
+echo created images (thumbs)
+echo.
+
+goto :eof
+
+
+:jekyll
+::
+:: Dateien für jekyll erstellen // files for jekyll
+::
 
 :: md-Datei erstellen // create .md
-Type NUL >%FolderName%.md
->>%FolderName%.md echo ---
->>%FolderName%.md echo title: %CurrDirName%
->>%FolderName%.md echo date: %date:~6,4%-%date:~3,2%-%date:~0,2%
->>%FolderName%.md echo ---
->>%FolderName%.md echo.
+::Type NUL >%FolderName%\%FolderName%.md
+::>>%FolderName%\%FolderName%.md echo ---
+::>>%FolderName%\%FolderName%.md echo title: "%CurrDirName%"
+::>>%FolderName%\%FolderName%.md echo date: %date:~6,4%-%date:~3,2%-%date:~0,2%
+::>>%FolderName%\%FolderName%.md echo album_folder: "%FolderName%/"
+::>>%FolderName%\%FolderName%.md echo ---
+::>>%FolderName%\%FolderName%.md echo.
+::echo created %FolderName%.md
+Type NUL >%FolderName%\index.md
+>>%FolderName%\index.md echo ---
+>>%FolderName%\index.md echo title: "%CurrDirName%"
+>>%FolderName%\index.md echo date: %date:~6,4%-%date:~3,2%-%date:~0,2%
+>>%FolderName%\index.md echo album_folder: /%FolderName%
+>>%FolderName%\index.md echo ---
+>>%FolderName%\index.md echo.
+echo created index.md
 
 :: Bilderliste in Markdown // image list in markdown
-for /F "tokens=*" %%f in ('dir /b thumbs') do (
- >>%FolderName%.md echo ![Bild]^({{site.imgpath}}%%f^)
+for /F "tokens=*" %%f in ('dir /b %FolderName%\thumbs') do (
+ >>%FolderName%\index.md echo [![Bild]^(%imgpath%/thumbs/%%f^)]^(%imgpath%/large/%%f^)
 )
 
 :: yml-Datei erstellen // create .yml
-Type NUL >%FolderName%.yml
-for /F "tokens=*" %%f in ('dir /b thumbs') do (
- >>%FolderName%.yml echo - %%f
+Type NUL >%FolderName%\%FolderName%.yml
+for /F "tokens=*" %%f in ('dir /b %FolderName%\thumbs') do (
+ >>%FolderName%\%FolderName%.yml echo - %%f
 )
+echo created %FolderName%.yml
+
+goto :eof
+
+:removetempfiles
+rd /s /q %tempFolder%
+goto :eof
+
+:end
